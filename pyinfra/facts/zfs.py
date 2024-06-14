322ab1ed45cf15daca44ce56876dd4e61b8689f0
James Taliaferro <james@taliafer.ro>
2024-06-13 20:46:17 -0700

    convert the other facts to ShortFact

--- pyinfra/facts/zfs.py
+++ pyinfra/facts/zfs.py
@@ -2,36 +2,49 @@
 Manage ZFS filesystems.
 """
 
-from pyinfra.api import FactBase
+from pyinfra.api import FactBase, ShortFactBase
 
 
-class _ZFSResourceBase(FactBase):
-    default = dict
+def _process_zfs_props_table(output):
+    datasets = {}
+    for line in output:
+        dataset, property, value, source = tuple(line.split("\t"))
+        if dataset not in datasets:
+            datasets[dataset] = {}
+        datasets[dataset][property] = value
+    return datasets
 
+
+class Pools(FactBase):
+    command = "zpool get -H all"
     @staticmethod
     def process(output):
-        datasets = {}
-        for line in output:
-            dataset, property, value, source = tuple(line.split("\t"))
-            if dataset not in datasets:
-                datasets[dataset] = {}
-            datasets[dataset][property] = value
-        return datasets
+        return _process_zfs_props_table(output)
 
 
-class Pools(_ZFSResourceBase):
-    command = "zpool get -H all"
-
-class Datasets(_ZFSResourceBase):
+class Datasets(FactBase):
     command = "zfs get -H all"
+    @staticmethod
+    def process(output):
+        return _process_zfs_props_table(output)
 
-class Filesystems(_ZFSResourceBase):
-    command = "zfs get -t filesystem -H all"
+class Filesystems(ShortFactBase):
+    fact=Datasets
+
+    @staticmethod
+    def process_data(data):
+        return {name:props for name, props in data.items() if props.get("type") == "filesystem"}
 
 
-class Snapshots(_ZFSResourceBase):
-    command = "zfs get -t snapshot -H all"
+class Snapshots(ShortFactBase):
+    fact=Datasets
+    @staticmethod
+    def process_data(data):
+        return {name:props for name, props in data.items() if props.get("type") == "snapshot"}
 
 
-class Volumes(_ZFSResourceBase):
-    command = "zfs get -t volume -H all"
+class Volumes(ShortFactBase):
+    fact=Datasets
+    @staticmethod
+    def process_data(data):
+        return {name:props for name, props in data.items() if props.get("type") == "volume"}
